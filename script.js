cd2af7ac8e028dc725d62f33dee784a2180fb140
anap <ana.souza9@ba.estudante.senai.br>
2022-10-07 10:47:23 -0300

    Meu Projeto

---
+++ script.js
@@ -0,0 +1,28 @@
+function enviar(){
+    // nome
+    let nomeLabel = document.getElementById('nomeLabel')
+
+    // email
+    let emailLabel = document.getElementById('emailLabel')
+
+    // let ddd = document.querySelector('#ddd')
+    // let 
+
+    // numero
+    let numLabel = document.getElementById('numLabel')
+
+    // experiencia
+    let experiencia = document.getElementById('experiencia')
+
+    // let listaContatos = []
+
+    let contato = {
+        nome: nomeLabel,
+        email: emailLabel,
+        num: numLabel,
+        exp: experiencia
+    }
+
+    // falta criar array
+    console.log(contato)
+}
